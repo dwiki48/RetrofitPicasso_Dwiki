8762f4f72d461f44dee7f2c5b41642f5e3309793
Dwiki Nur Adiansyah <adnsyhdwiki69@gmail.com>
2019-05-03 14:43:35 +0700

    Initial commit

---
+++ app/src/main/java/com/appmaintenance/kino/retrofit/activity/MainActivity.java
@@ -0,0 +1,61 @@
+package com.appmaintenance.kino.retrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.appmaintenance.kino.retrofit.R;
+import com.appmaintenance.kino.retrofit.adapter.MoviesAdapter;
+import com.appmaintenance.kino.retrofit.model.Movie;
+import com.appmaintenance.kino.retrofit.model.MovieResponse;
+import com.appmaintenance.kino.retrofit.rest.MovieApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+    private RecyclerView recyclerView = null;
+    private final static String API_KEY = "314a1bdae0204f2cab0992a69a1ad6b4";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        connectAndGetApiData();
+    }
+
+    public void connectAndGetApiData(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        MovieApiService movieApiService = retrofit.create(MovieApiService.class);
+        Call<MovieResponse> call = movieApiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MovieResponse>() {
+            @Override
+            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+                Log.d(TAG, "Jumlah film: " + movies.size());
+            }
+            @Override
+            public void onFailure(Call<MovieResponse> call, Throwable throwable) {
+                Log.e(TAG, throwable.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/appmaintenance/kino/retrofit/rest/MovieApiService.java
@@ -0,0 +1,12 @@
+package com.appmaintenance.kino.retrofit.rest;
+
+import com.appmaintenance.kino.retrofit.model.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApiService {
+    @GET("movie/top_rated")
+    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+}
